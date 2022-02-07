97cfe078505ecc94a8f412bf06bb1a5266f8a7b4
Gino Brunner <gino_brunner@mckinsey.com>
2022-02-07 14:27:52 +0100

    get day of week for filtering

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -23,7 +23,7 @@ def get_filters():
     month = input("Enter month for filtering: ")
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
+    day_of_week = input("Enter day of week for filtering: ")
 
     print('-'*40)
     return city, month, day
